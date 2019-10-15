702697fc0cca4b32de2f876b1b33939e09a8d65c
Malte <malte.heinrich95@gmx.de>
2019-10-15 20:17:22 +0200

    CHanged master node the way that one can pass this executeable the arguments for input and output topic of the master

--- skripts/robot_master.py
+++ skripts/robot_master.py
@@ -2,9 +2,10 @@
 
 import rospy
 import class_controller
+import sys
 
 if __name__=="__main__":    
    ctr=class_controller.RobotController(frequenzy=5)
-   ctr.link_input_topic(rospy.get_param("input_master"))
-   ctr.link_output_topic(rospy.get_param("output_master"))
+   ctr.link_input_topic(sys.argv[1])
+   ctr.link_output_topic(sys.argv[2])
    ctr.execute()
