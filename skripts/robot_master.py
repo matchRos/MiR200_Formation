3413e97355d0d5ee23f455bb9577bf58cb74d793
RosMatch <malte.heinrich95@gmx.de>
2019-10-16 11:36:01 +0200

    made master flexible in terms of given paths. Master can now be launched wit rectangular path, circle path or without path (user publishing)

--- skripts/robot_master.py
+++ skripts/robot_master.py
@@ -4,8 +4,18 @@ import rospy
 import class_controller
 import sys
 
-if __name__=="__main__":    
-   ctr=class_controller.RobotController(frequenzy=5)
-   ctr.link_input_topic(sys.argv[1])
-   ctr.link_output_topic(sys.argv[2])
-   ctr.execute()
+if __name__=="__main__": 
+    ctr_type=sys.argv[3]
+  
+         
+    if ctr_type=="quad":
+         ctr=class_controller.PathPlannerQuadratic()
+    elif ctr_type=="circ":
+         ctr=class_controller.PathPlannerCircular()
+    else:
+        ctr=class_controller.RobotController()
+   
+    ctr.set_frequenzy(rospy.get_param("/frequenzy"))
+    ctr.link_input_topic(sys.argv[1])
+    ctr.link_output_topic(sys.argv[2])
+    ctr.execute()
