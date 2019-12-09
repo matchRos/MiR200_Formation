2b7d19a48e80597b3c60c2a6d27dc9b1222ec76d
ibMH <malte.heinrich95@gmx.de>
2019-12-09 10:51:17 +0100

    Changed lyapunov call for new parameters

--- controller/src/master.cpp
+++ controller/src/master.cpp
@@ -15,8 +15,8 @@ void Master::scope()
             this->calc_Lyapunov(    this->lyapunov_parameter.kx,
                                     this->lyapunov_parameter.ky,
                                     this->lyapunov_parameter.ktheta,
-                                    this->lyapunov_parameter.omega,
-                                    this->lyapunov_parameter.v);
+                                    0.2,
+                                    0.2);
             break;
         case angle_distance: this->calc_angle_distance(this->kr,this->kang);break;
         default: 
