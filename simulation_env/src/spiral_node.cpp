d415ae5a13f3f326357f303e3e7f7d62ec0a07d5
Malte <Malte.Heinrich95@gmx.de>
2020-01-17 19:44:02 +0100

    replaced clothoide node by spiral node

---
+++ simulation_env/src/spiral_node.cpp
@@ -0,0 +1,11 @@
+#include <simulation_env/planner.h>
+
+
+int main (int argc,char**argv)
+{
+    ros::init(argc,argv,"SpiralPlanner");
+    ros::NodeHandle nh;    
+    Spiralplanner euler(nh);
+    euler.set_parameter(3.0,1.0/(2*M_PI),0.1);
+    ros::spin(); 
+}
