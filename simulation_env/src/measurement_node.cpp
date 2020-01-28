553ad0142ff1685ee9385ae4fe09183eaf25c676
Malte <Malte.Heinrich95@gmx.de>
2020-01-28 12:54:01 +0100

    Deleted robot 3 and 4

--- simulation_env/src/measurement_node.cpp
+++ simulation_env/src/measurement_node.cpp
@@ -47,46 +47,31 @@ int main(int argc,char** argv)
 
     Formation::RobotProperties properties_master;
     properties_master.name="robot_master";
-    properties_master.neighbours=Formation::Neighbours{"robot1","robot2","robot3","robot4"};
+    properties_master.neighbours=Formation::Neighbours{"robot1","robot2"};
     properties_master.pose=tf::Pose(tf::createIdentityQuaternion(),tf::Vector3(0.0,0.0,0.0));
     properties_master.laser_frames=frames;
     properties_master.laser_topics=topics;
 
     Formation::RobotProperties properties_robot1;
     properties_robot1.name="robot1";
-    properties_robot1.neighbours=Formation::Neighbours{"robot_master","robot2","robot3","robot4"};
+    properties_robot1.neighbours=Formation::Neighbours{"robot_master","robot2"};
     properties_robot1.pose=tf::Pose(tf::createIdentityQuaternion(),tf::Vector3(-1.5,1.5,0.0));
     properties_robot1.laser_frames=frames;
     properties_robot1.laser_topics=topics;
 
     Formation::RobotProperties properties_robot2;
     properties_robot2.name="robot2";
-    properties_robot2.neighbours=Formation::Neighbours{"robot_master","robot1","robot3","robot4"};
+    properties_robot2.neighbours=Formation::Neighbours{"robot_master","robot1"};
     properties_robot2.pose=tf::Pose(tf::createIdentityQuaternion(),tf::Vector3(-1.5,-1.5,0.0));
     properties_robot2.laser_frames=frames;
     properties_robot2.laser_topics=topics;    
 
-    Formation::RobotProperties properties_robot3;
-    properties_robot3.name="robot3";
-    properties_robot3.neighbours=Formation::Neighbours{"robot_master","robot2","robot1","robot4"};
-    properties_robot3.pose=tf::Pose(tf::createIdentityQuaternion(),tf::Vector3(-0.5,-3.5,0.0));
-    properties_robot3.laser_frames=frames;
-    properties_robot3.laser_topics=topics;
-
-    Formation::RobotProperties properties_robot4;
-    properties_robot4.name="robot4";
-    properties_robot4.neighbours=Formation::Neighbours{"robot_master","robot2","robot1","robot3"};
-    properties_robot4.pose=tf::Pose(tf::createIdentityQuaternion(),tf::Vector3(-5.5,-3.5,0.0));
-    properties_robot4.laser_frames=frames;
-    properties_robot4.laser_topics=topics;
 
     Formation test;
     test.addRobot(properties_master);
     test.addRobot(properties_robot1);
     test.addRobot(properties_robot2);
-    test.addRobot(properties_robot3);
-    test.addRobot(properties_robot4);
-    test.startPrediction(10);
+    test.startPrediction(1);
 
     FormationPublisher pub(&test);
 
