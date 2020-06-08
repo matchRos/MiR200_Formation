5ab24876fdccc287955a424268436f3378587580
Heinrich <Heinrich>
2020-06-08 17:05:32 +0200

    Reorganzed order of transformations in target calc

--- multi_robot_controller/src/slave.cpp
+++ multi_robot_controller/src/slave.cpp
@@ -33,10 +33,7 @@ void Slave::targetOdomCallback(nav_msgs::Odometry msg)
     ang.setY(0.0);
     tf::Vector3 lin;
 
-    //Get necessary transformations
-    tf::Transform trafo;  
-    tf::poseMsgToTF(msg.pose.pose,trafo);
-    tf::Transform rot(trafo.getRotation(),tf::Vector3(0.0,0.0,0.0));
+    
     
     //Get the transformation for frames 
     tf::StampedTransform trafo_frames;
@@ -47,7 +44,12 @@ void Slave::targetOdomCallback(nav_msgs::Odometry msg)
     {
         ROS_WARN("%s",ex.what());
     }
+    //Get necessary transformations
+    tf::Transform trafo;  
+    tf::poseMsgToTF(msg.pose.pose,trafo);
     trafo=trafo_frames*trafo;
+    tf::Transform rot(trafo.getRotation(),tf::Vector3(0.0,0.0,0.0));
+   
 
     //Get the current velocity
     VelocityEulerian vel_eul;
@@ -96,6 +98,7 @@ void Slave::targetOdomCallback(nav_msgs::Odometry msg)
                 if(delta_angle>0.9*2*M_PI){delta_angle=delta_angle-2*M_PI;}
                 if(delta_angle<-0.9*2*M_PI){delta_angle=delta_angle+2*M_PI;}
                 this->target_state_.angular_velocity=delta_angle/(msg.header.stamp.toSec()-this->time_old_);
+                // this->target_state_.angular_velocity=(this->target_state_.velocity.x()*acc.y()-this->target_state_.velocity.y()*acc.x())/this->target_state_.velocity.length2();
                 this->old_angle_=angle; 
             }
             else
@@ -122,7 +125,7 @@ void Slave::targetOdomCallback(nav_msgs::Odometry msg)
         
         case ControllerType::angle_distance:
         {            
-            tf::poseMsgToTF(msg.pose.pose,this->target_state_.pose);
+            this->target_state_.pose=trafo;
             this->target_state_.velocity=lin;
             this->target_state_.angular_velocity=ang.z();
             break;
@@ -149,15 +152,6 @@ Controller::ControlVector Slave::calcAngleDistance(AngleDistanceParameter parame
 
     float psi12=atan2(r.y(),r.x());
     if(psi12<0.0){psi12+=2*M_PI;}
-    ROS_INFO("target: %lf, %lf ,%lf,%lf",   master_reference_.getOrigin().x(),
-                                            master_reference_.getOrigin().y(),
-                                            target.pose.getOrigin().x(),
-                                            target.pose.getOrigin().y());
-
-    ROS_INFO("current: %lf,%lf,%lf,%lf",    r.x(),
-                                            r.y(),
-                                            current.pose.getOrigin().x(),
-                                            current.pose.getOrigin().y());
 
     float omega1=target.angular_velocity;
     float omega2=current.angular_velocity;
@@ -168,9 +162,7 @@ Controller::ControlVector Slave::calcAngleDistance(AngleDistanceParameter parame
     float gamma1=theta1+psi12-theta2;
     float roh12=(parameter.linear_gain*(l12d-l12)+v1*cos(psi12))/cos(gamma1);
 
-    this->control_dif_.setOrigin(tf::Vector3(l12d-l12,0,0));
-    this->control_dif_.setRotation(tf::createQuaternionFromYaw(psi12d-psi12));
-    ControlVector u;
+    ControlVector u;    
     u.omega=cos(gamma1)/parameter.d*(parameter.angular_gain*l12*(psi12d-psi12)-v1*sin(psi12)+l12*omega1+roh12*sin(gamma1));
     u.v=roh12-parameter.d*omega2*tan(gamma1);
 
